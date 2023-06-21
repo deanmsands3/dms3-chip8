16c4ad347f24f962407966d98e7cc3c07c11618e
Dean M. Sands III <deanmsands3@gmail.com>
2023-06-21 13:03:35 -0500

    Slowly building up the Timer class. Also, CLion doesn't like the 'm_' prefix, and I kinda hate it myself.

--- src/Timer.cpp
+++ src/Timer.cpp
@@ -10,16 +10,58 @@
 
 #pragma region Includes
 #include "Timer.h"
+
+#include <utility>
 #pragma endregion Includes
 
 #pragma region Public Methods
-    Timer(float frequency, std::function<void()> callback);
-    virtual ~Timer();
+namespace DMS3 {
+    Timer::Timer(float frequency, const std::function<void()> &callback): _callback(callback){
+        setFrequency(frequency);
+
+    }
+    Timer::~Timer(){
+
+    }
+
+    void Timer::setFrequency(float frequency) {
+        std::unique_lock<std::mutex> lock(_mutex);
+        _frequency = frequency;
+    }
+
+    float Timer::getFrequency() const {
+        std::unique_lock<std::mutex> lock(_mutex);
+        return _frequency;
+    }
+
+    uint8_t Timer::getCount() const {
+        std::unique_lock<std::mutex> lock(_mutex);
+        return _count;
+    }
+
+    void Timer::setCount(uint8_t count) {
+        std::unique_lock<std::mutex> lock(_mutex);
+        _count = count;
+    }
+
+    const std::function<void()> &Timer::getCallback() const {
+        return _callback;
+    }
+
+    void Timer::setCallback(const std::function<void()> &callback) {
+        _callback = callback;
+    }
+    void Timer::stop() {
+
+    }
 #pragma endregion Public Methods
 
 #pragma region Private Methods
+    void Timer::_thread_function() {
 
+    }
 #pragma endregion Private Methods
+};
 
 
 
--- src/Timer.h
+++ src/Timer.h
@@ -11,18 +11,35 @@
 #ifndef DMS3_CHIP8_TIMER_H
 #define DMS3_CHIP8_TIMER_H
 #pragma region Includes
+#include "Types.h"
 #include <functional>
+#include <chrono>
+#include <thread>
+#include <mutex>
 #pragma endregion Includes
 
 namespace DMS3{
-class Timer {
-	float _frequency;
-	std::function<void()> _callback;
-public:
-    
-    Timer(float frequency, std::function<void()> callback);
-    virtual ~Timer();
-};
+    class Timer {
+    private:
+        std::chrono::microseconds _timeout;
+        uint8_t _count;
+        float _frequency;
+        std::function<void()> _callback;
+        std::thread _thread;
+        mutable std::mutex _mutex;
+        std::condition_variable _cv;
+        void _thread_function();
+    public:
+        Timer(float frequency, const std::function<void()> &callback);
+        virtual ~Timer();
+        [[nodiscard]] float getFrequency() const;
+        void setFrequency(float frequency);
+        [[nodiscard]] const std::function<void()> &getCallback() const;
+        void setCallback(const std::function<void()> &callback);
+        [[nodiscard]] uint8_t getCount() const;
+        void setCount(uint8_t count);
+        void stop();
+    };
 }	// DMS3
 
 #endif //DMS3_CHIP8_TIMER_H
